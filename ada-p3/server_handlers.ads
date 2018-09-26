e688c866d7d06d4b3a1d6c1b6f01f2fb8b31f225
Álvaro del Hoyo <32258439+alvarodh@users.noreply.github.com>
2018-09-26 12:43:46 +0200

    Update server_handlers.ads

--- ada-p3/server_handlers.ads
+++ ada-p3/server_handlers.ads
@@ -1,1 +1,15 @@
+with Lower_Layer_UDP;
 
+package Server_Handlers is
+
+    package LLU renames Lower_Layer_UDP;
+
+    procedure Handler (From     : in  LLU.End_Point_Type;
+                       To       : in  LLU.End_Point_Type;
+                       P_Buffer : access LLU.Buffer_Type);
+
+    procedure Print_Active_Clients;
+
+    procedure Print_Old_Clients;
+
+end Server_Handlers;
